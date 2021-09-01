c685398e9165e9f428a8b588d52811ceb19c1f6d
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2021-09-01 17:26:10 -0300

    add e retirada de pessoas

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ int main() {
 	readJsonCategoria(&LC);
 	readJsonEstante(&LE);
 	//pessoasNaBibioteca();
+	//saidaDePessoas(int id);
 
 	do {
         system("cls || clear");
@@ -74,4 +75,34 @@ int main() {
 			cout << "Por favor espere lá fora ate alguem sair!"  << endl;
 		}
 	}
+
+//inicializa a biblioteca cheia
+		List<int>IdPessoas;
+		node<int> *pId = L->HEAD;
+
+		for(int i = 0; i < 10; i++){
+			IdPessoas.push(i);
+		}
+
+//função que inicializa com a biblioteca cheia e vai se modificando a medida que alguem sai
+	void pessoasNaBiBlioteca(){
+		int cont = IdPessoas.size();
+		if(cont <= 10){
+			IdPessoas.push(rand%100);
+			cont++;
+		}else{
+			cout << "\tBIBLIOTECA LOTADA!" << endl;
+			cout << "Por favor espere lá fora ate alguem sair!"  << endl;
+		}
+	}
+	void saidaDePessoas(int id){
+		int aux;
+		for(int i = 0; i < IdPessoas.size(); i++){
+			x = IdPessoas->dado;
+			pId = pId->prox;
+			if(x == id){
+				IdPessoas.pop();
+			}
+		}
+	}	
 */
